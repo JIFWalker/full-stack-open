c6ac8df8009cb2ba2eb33240a430e37928f35563
JIFWalker <jiwww.walker@gmail.com>
2021-06-13 14:34:12 -0700

    part 2,8

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -2,34 +2,55 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' },
-
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
-
-  const addName = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name : newName
-    }
-    persons.map(person => person.name).includes(personObject.name)
-      ? alert(`${newName} is already added to phonebook`) 
-      : setPersons(persons.concat(personObject))
-        setNewName('')
-  }
+  const [ newNum, setNewNum ] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumChange = (event) => (
+    setNewNum(event.target.value)
+  )
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name : newName,
+      number : newNum
+    }
+    switch (true) {
+      case persons.map(person => person.name).includes(personObject.name):
+        alert(`${newName} is already added to phonebook`)
+        break
+      case persons.map(person => person.number).includes(personObject.number):
+        alert(`${newNum} is already added to phonebook`)
+        break
+      default:
+       setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+    setNewNum('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit = {addName}>
+      <form onSubmit = {addPerson}>
         <div>
           name: <input 
             value = {newName}
             onChange = {handleNameChange}
+            />
+        </div>
+        <div>
+          number: <input
+            value = {newNum}
+            onChange = {handleNumChange}
             />
         </div>
         <div>
@@ -39,7 +60,7 @@ const App = () => {
       <h2>Numbers</h2>
         {persons.map(person =>   
           <ul key = {person.name} >
-            {person.name}
+            {person.name}: {person.number}
           </ul>
         )}
     </div>
