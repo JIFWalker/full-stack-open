02c9a40f31fd7b3c209b6b36e211088d31642deb
JIFWalker <jiwww.walker@gmail.com>
2021-08-04 13:50:22 -0700

    part 4.13

--- part04/blog-list/controllers/blogs.js
+++ part04/blog-list/controllers/blogs.js
@@ -21,13 +21,18 @@ blogsRouter.post('/', async (request, response) => {
         url: body.url,
         likes: body.likes || 0
     })
-    if (blog.title === undefined || blog.url === undefined) {
+    if (!blog.title || !blog.url) {
         await response.status(400).end()
     } else {
         const savedBlog = await blog.save()
         response.status(201).json(savedBlog)
         console.log('added following to database', savedBlog)
     }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part04/blog-list/tests/blog-api.test.js
+++ part04/blog-list/tests/blog-api.test.js
@@ -8,69 +8,94 @@ const Blog = require('../models/blog')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(helper.initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(helper.initialBlogs[1])
-    await blogObject.save()
+    console.log('database cleared')
+
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+    console.log('database initialization complete')
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-    console.log('blog is in JSON format')
+describe('when there is some blogs alredy saved', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+        console.log('blog is in JSON format')
 
-    const response = await api.get('/api/blogs')
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-    console.log(`there are ${response.body.length} blogs`)
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+        console.log(`there are ${response.body.length} blogs`)
+    })
+
+    test('blog unique ID is named "id"', async () => {
+        const response = await api.get('/api/blogs')
+        response.body.map(blog => expect(blog).toHaveProperty('id'))
+
+})
 })
 
-test('blog unique ID is named "id"', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.map(blog => expect(blog).toHaveProperty('id'))
+describe('when making a POST request', () => {
+    test('HTTP POST request creates new blog post', async () => {
+        const newBlog = {
+            _id: '5a422bc61b54a676234d17fc',
+            title: 'Spice & Wolf',
+            author: 'Holo',
+            url: 'http://blog.spicenadwolf.com',
+            likes: 666,
+            __v: 0
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
+        const response = await api.get('/api/blogs')
+        expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    })
+
+    test('"likes" property is not missing from the request', async () => {
+        const blogs = await api.get('/api/blogs')
+
+        blogs.body.map(blog => expect(blog).toHaveProperty('likes'))
+    })
+
+    test('verified request with missing title and url properties returns 400', async () => {
+        const newBlog = {
+            _id: '5a422bc61b54a676234d17fc',
+            author: 'Holo',
+            likes: 666,
+            __v: 0
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+    })
 })
 
-test('HTTP POST request creates new blog post', async () => {
-    const newBlog = {
-        _id: '5a422bc61b54a676234d17fc',
-        title: 'Spice & Wolf',
-        author: 'Holo',
-        url: 'http://blog.spicenadwolf.com',
-        likes: 666,
-        __v: 0
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('when deleting a specific blog entry', () => {
+    test('delete single blog entry from database', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
 
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(
+            helper.initialBlogs.length - 1
+        )
+    })
 })
 
-test('"likes" property is not missing from the request', async () => {
-    const blogs = await api.get('/api/blogs')
-
-    blogs.body.map(blog => expect(blog).toHaveProperty('likes'))
-})
-
-test('verified request with missing title and url properties returns 400', async () => {
-    const newBlog = {
-        _id: '5a422bc61b54a676234d17fc',
-        author: 'Holo',
-        likes: 666,
-        __v: 0
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-})
 afterAll(() => {
     mongoose.connection.close()
 })
