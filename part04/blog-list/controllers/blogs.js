d3ca13c06b4384a8a509372d84c5f1bc719659ba
JIFWalker <jiwww.walker@gmail.com>
2021-08-12 16:48:51 -0700

    part 4.20

--- part04/blog-list/controllers/blogs.js
+++ part04/blog-list/controllers/blogs.js
@@ -4,13 +4,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 require('express-async-errors')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
+
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -26,9 +20,8 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
 
