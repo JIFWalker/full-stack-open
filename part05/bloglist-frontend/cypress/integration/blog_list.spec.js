c4160f8e40d1b56167dd91aa45aa869c3f75dfa9
JIFWalker <jiwww.walker@gmail.com>
2021-10-28 15:39:11 -0700

    part 5.18

--- part05/bloglist-frontend/cypress/integration/blog_list.spec.js
+++ part05/bloglist-frontend/cypress/integration/blog_list.spec.js
@@ -3,10 +3,38 @@
 describe('Blog app', function() {
     beforeEach(function() {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+        const user = {
+            name: 'Holo',
+            username: 'WiseWolf',
+            password: 'Apples'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
     it('Login form is shown', function() {
         cy.contains('Log In To Application')
+    })
+
+    describe('Login', function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('WiseWolf')
+            cy.get('#password').type('Apples')
+            cy.get('#login-button').click()
+
+            cy.contains('Login Succesful!')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.get('#username').type('Wisewolf')
+            cy.get('#password').type('Onions')
+            cy.get('#login-button').click()
+
+            cy.get('.error')
+                .should('contain', 'Error')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+            cy.get('html').should('not.contain', 'Holo has logged in')
+        })
     })
 })
