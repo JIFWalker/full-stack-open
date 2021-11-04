bd7c20c220c7e3e9b8d48521f03cb3592dbf29f0
JIFWalker <jiwww.walker@gmail.com>
2021-11-04 14:26:18 -0700

    part 6.6 and 6.7

--- part06/redux-anecdotes/src/App.js
+++ part06/redux-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { vote } from './reducers/anecdoteReducer'
-import NewAnecdote from './components/NewAnecdote'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -25,7 +25,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-        <NewAnecdote />
+        <AnecdoteForm />
     </div>
   )
 }
