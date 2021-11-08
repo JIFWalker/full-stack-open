23cd96c6c7aedf0e85fb4a2ef978969cae51b1c1
JIFWalker <jiwww.walker@gmail.com>
2021-11-08 13:15:22 -0800

    part 6.13

--- part06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -17,11 +10,11 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch(action.type) {
+    case 'anecdote/init':
+      return action.payload
+
     case 'anecdote/new':
     return state.concat(action.payload)
 
@@ -51,6 +44,13 @@ export const createAnecdote = (content) => {
   return {
     type: 'anecdote/new',
     payload: asObject(content)
+  }
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'anecdote/init',
+    payload: anecdotes
   }
 }
 
