945858135e5b8cd01b7ea598836db3b707ddc1c3
JIFWalker <jiwww.walker@gmail.com>
2022-04-29 14:18:42 -0700

    part 8.3

--- part08/library-backend/index.js
+++ part08/library-backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { argsToArgsConfig } = require('graphql/type/definition')
 
 let authors = [
   {
@@ -95,10 +96,18 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
   }
+
+  type Author {
+      name: String!
+      born: Int
+      bookCount: Int!
+  }
+
   type Query {
       bookCount: Int!
       authorCount: Int!
       allBooks: [Book!]!
+      allAuthors: [Author!]!
   }
 `
 
@@ -107,6 +116,10 @@ const resolvers = {
       bookCount: () => books.length,
       authorCount: () => authors.length,
       allBooks: () => books,
+      allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
 
