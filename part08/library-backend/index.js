7c5d188b1a58ca1eb0f0cb6795a81451fc3dcd33
JIFWalker <jiwww.walker@gmail.com>
2022-04-29 15:03:27 -0700

    part 8.4

--- part08/library-backend/index.js
+++ part08/library-backend/index.js
@@ -106,7 +106,7 @@ const typeDefs = gql`
   type Query {
       bookCount: Int!
       authorCount: Int!
-      allBooks: [Book!]!
+      allBooks(author: String): [Book!]!
       allAuthors: [Author!]!
   }
 `
@@ -115,12 +115,20 @@ const resolvers = {
   Query: {
       bookCount: () => books.length,
       authorCount: () => authors.length,
-      allBooks: () => books,
+      allBooks: (root, args) => {
+        if (!args.author) {
+          return books
+        }
+        return books.filter(b => b.author === args.author)
+      },
       allAuthors: () => authors,
   },
   Author: {
-    bookCount: (root) => books.filter(b => b.author === root.name).length
-  }
+    bookCount: (root) => 
+      books.filter(b => 
+        b.author === root.name
+      ).length,
+  },
 }
 
 const server = new ApolloServer({
